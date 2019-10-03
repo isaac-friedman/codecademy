05fc5557b235562c2c8febec009f083a5cab8a95
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-03 16:29:44 -0400

    examples using various properties of JS string object

---
+++ javascript/length_examples.js
@@ -0,0 +1,11 @@
+//Generating html in strings is generally a bad idea without a good rason
+document.getElementById("body").innerHTML = "Check your console to understand this exercise."
+
+let demoString = "This string is just long enough to be inconvenient to count by looking at it"
+
+console.log(demoString + \n + "demoString is " + demoString.length + " characters long.")
+
+console.log("The Nth character of demoString is located at demoString.N. For example if N = 17")
+console.log(demostring.17)
+
+
