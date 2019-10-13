cc2af955c06a855a1a7787c1384f4489b01ee2ab
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-13 14:43:24 -0400

    add: compareGuesses() function

--- javascript/Number Guesser/script.js
+++ javascript/Number Guesser/script.js
@@ -5,3 +5,19 @@ let currentRoundNumber = 1;
 function generateTarget() {
     return Math.floor(Math.random()*10);
 }
+
+function compareGuesses(human, confuser, secret) {
+    let humanWins = null;
+    //Calculate difference between user and target
+    let humanDiff = Math.abs(target - human);
+    let compDiff = Math.abs(target - confuser);
+
+    /*I think that true and false is a poor (counter-intuitive) design choice
+    for determining who wins a game but that is part of the project spec.*/
+
+    if (humanDiff <= compDiff) {
+        humanWins = true;
+    } else {//Ties are thrown for the human player
+        humanWins = false;
+    }
+    return humanWins;
