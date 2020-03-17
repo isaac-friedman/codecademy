da12543d312d828c9c3cbfbc80bf3c340b2ce94e
Isaac Friedman <isaac@isaac-friedman.com>
2020-03-17 19:29:54 -0400

    Book class

--- javascript/Chapter 8 Classes and Inheritance/library.js
+++ javascript/Chapter 8 Classes and Inheritance/library.js
@@ -24,3 +24,19 @@ class Media {
       this._ratings.push(rating);
   }
 }
+
+class Book extends Media {
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+}
