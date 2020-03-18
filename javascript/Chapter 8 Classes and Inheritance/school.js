0c1c762f80aef869e69c15429585ddcda59ca717
Isaac Friedman <isaac@isaac-friedman.com>
2020-03-17 20:08:58 -0400

    Add parent class constructor

--- javascript/Chapter 8 Classes and Inheritance/school.js
+++ javascript/Chapter 8 Classes and Inheritance/school.js
@@ -1,4 +1,30 @@
 class School {
+    constructor(name, level) {
+        const validLevels = ['primary', 'middle', 'high'];
+        //first validate the level
+        if(!validLevels.includes(level)) {
+            throw "Invalid school type";
+        }
+        this._name = name;
+        this._level = level;
+        this._studentCount = 0;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get studentCount() {
+        return this._studentCount;
+    }
+
+    set studentCount(num) {
+        this._studentCount;
+    }
 
 }
 
@@ -11,5 +37,5 @@ class Middle extends School {
 }
 
 class High extends School {
-    
+
 }
